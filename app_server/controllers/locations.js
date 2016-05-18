e73499f7e65de30c9fc55f93a76d6223bec2235d
ConorONeill91 <conor.oneill63@mail.dcu.ie>
2016-05-18 22:52:34 +0100

    Hard coded views

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+/* GET 'home' page */
+module.exports.homelist = function(req,res){
+  res.render('locations-list', {title: 'Home'});
+};
+
+/* GET 'Location info' page */
+module.exports.locationInfo = function(req,res){
+  res.render('location-info', {title: 'Location info'});
+};
+
+/* GET 'Add Review' page */
+module.exports.addReview = function(req,res){
+  res.render('index', {title: 'Add Review'});
+};
